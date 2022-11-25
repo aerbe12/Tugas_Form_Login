f4adada6f02440743b67cd34da3643516f298360
aerbe12 <arya.wirya12@gmail.com>
2022-11-25 16:17:36 +0800

    kedua

--- loginact.php
+++ loginact.php
@@ -1,30 +1,26 @@
 <?php
 
+    session_start();
+    if (empty($_SESSION['username'])) {
+        if(!empty($_POST['username']) && !empty($_POST['password'])){
+            $data['username'] = $_POST['username'];
+            $data['password'] = $_POST['password'];
 
-$database = file_get_contents("http://localhost/latphp/data.json");
-$database = json_decode($database, TRUE);
-if(isset($_POST['username']) && isset($_POST['password'])) {
-    for($i=0; $i < count($database); $i++) {
-        if($database[$i]['username'] == $_POST['username']) {
-            if($database[$i]['password'] == $_POST['password']) {
-                $success = TRUE;
-                break;
-            } else {
-                $success = FALSE;
+            $myfile = fopen("data.json", "r") or die("Unable to open file!");
+            $credential = json_decode(fread($myfile,filesize("data.json")));
+            if($data['username'] == $credential->username && ($data['password']) == $credential->password){
+                $_SESSION["username"] = $data['username'];
+                header('Location:loginact.php');
+            }else{
+                echo"Login Gagal <br><br><a href='logout.php'>Kembali</a> ";
             }
-        } else {
-            $success = FALSE;
+            fclose($myfile);
+        }else{
+            echo "Email dan password Kosong";
         }
+    }else{
+        echo "Selamat Sudah Berhasil Login, ".$_SESSION["username"].
+        "<br><br><a href='logout.php'>Log Out</a>" ;
     }
-} else {
-    echo "Harap isi semua kolom yang tersedia";
-}
-if($success == TRUE) {
-    echo "Selamat Datang ".$_POST['username'];
-    echo '<br><br><a href="index.html">Log Out</a>';
-} else {
-    echo "Username/Password Salah";
-}
 ?>
-
 
